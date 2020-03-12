edc47f0f1be386a08c38d01d34b76ac4a31775f3
Marc Hernandez <marcsh@gmail.com>
2020-03-11 23:28:40 -0700

    Converted project

---
+++ Source/Settlement/SettlementGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SettlementGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SETTLEMENT_API ASettlementGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
